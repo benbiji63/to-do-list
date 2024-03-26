316086bb154f196a3e62eca647a71c207f1b72f2
anUnknownStoic <104714642+benbiji63@users.noreply.github.com>
2024-03-26 12:53:29 +0530

    Finished the project

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,47 @@
+const inputBox = document.getElementById('input-box');
+const list = document.getElementById('list');
+const add = document.getElementById('add');
+const form = document.getElementById('form');
 
+function storeData() {
+  localStorage.setItem('data', list.innerHTML);
+  console.log(localStorage.getItem('data'));
+}
+
+function showTask() {
+  list.innerHTML = localStorage.getItem('data');
+  console.log(localStorage.getItem('data'));
+}
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  let text = inputBox.value;
+  if (text === '') {
+    alert('You must enter something');
+  } else {
+    const newTodo = document.createElement('li');
+    newTodo.innerText = text;
+    let span = document.createElement('span');
+    span.innerText = '\u00d7';
+    newTodo.append(span);
+    list.append(newTodo);
+  }
+  inputBox.value = '';
+  storeData();
+});
+
+list.addEventListener(
+  'click',
+  e => {
+    if (e.target.tagName === 'LI') {
+      e.target.classList.toggle('checked');
+      storeData();
+    } else if (e.target.tagName === 'SPAN') {
+      e.target.parentElement.remove();
+      storeData();
+    }
+  },
+  false
+);
+
+showTask();
